894893f26492deebf066a662140e0bcb3cb4d006
ElvisStwar <elvis.stwar@hotmail.com>
2022-12-27 09:45:26 -0500

    Creacion de navbar y card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyModule } from './body/body.module';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    BodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
